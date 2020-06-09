60e88fdb09a8fecdcc62b2ce5665baaee2314cc1
aerendil <arielastudillo@outlook.es>
2020-06-08 21:25:15 -0400

    Se agrega ProveedorProducto model con inserts respectivos a la bd.

--- tienda/utils.py
+++ tienda/utils.py
@@ -1,6 +1,13 @@
 ## Archivo para dejar funciones de ambito general.
 
 def formatear_numero_miles(entero):
+    """Formatea un número entero con separadores de miles.
+
+    Args:
+        entero (int): entero a formatear
+    Returns:
+        String del entero con formato
+    """
     numero_formateado = f"{entero:,}"
 
     resultado = format(numero_formateado).replace(',', '.')
