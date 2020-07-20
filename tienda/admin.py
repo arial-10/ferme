801652613fba54f9c9ad20e88b889f635ce54fa7
Ariel Astudillo <arielastudillos@outlook.com>
2020-07-19 21:52:59 -0400

    Control de roles en ferme admin.

--- tienda/admin.py
+++ tienda/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Cliente)
+admin.site.register(Vendedor)
+admin.site.register(Empleado)
 admin.site.register(Producto)
 admin.site.register(Marca)
